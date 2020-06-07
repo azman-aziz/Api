e1f88ad6cb42d6c004561f7e63be699d55e070df
azman-aziz <am911444@gmail.com>
2020-06-07 10:23:33 +0700

    menambahkan catatan dan file latihan seri json

--- json/latihan1.php
+++ json/latihan1.php
@@ -1,16 +1,22 @@
 <?php
 
-$mahasiswa = [
-	[
-	"nama" => "aziz Maulana",
-	"alamat" => "127.0.0.1"
-	],
-	[
-	"nama" => "as Maulana",
-	"alamat" => "127.0.0.1"
-	]
+// $mahasiswa = [
+// 	[
+// 	"nama" => "aziz Maulana",
+// 	"alamat" => "127.0.0.1"
+// 	],
+// 	[
+// 	"nama" => "as Maulana",
+// 	"alamat" => "127.0.0.1"
+// 	]
 
-];
+// ];
 
-$data = json_encode($mahasiswa,true);
+$dbh = new PDO('mysql:host=localhost;dbname=phpmvc;', 'admin', 'password');
+$db = $dbh->prepare('SELECT * FROM mahasiswa');
+$db->execute();
+
+$mahasiswa = $db->fetchAll(PDO::FETCH_ASSOC);
+
+$data = json_encode($mahasiswa);
 var_dump($data);
